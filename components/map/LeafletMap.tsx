75821e202338d85b5cdff2a935b4e61bf0e89bcc
Fabien.LENOIR <fabien.lenoir@berger-levrault.com>
2026-04-20 14:56:19 +0200

    fix: restore Leaflet CSS import + CSP unpkg for marker icons (l'import CSS dans LeafletMap.tsx + unpkg.com dans la CSP)

--- components/map/LeafletMap.tsx
+++ components/map/LeafletMap.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from 'react'
 import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 import 'leaflet.markercluster'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
